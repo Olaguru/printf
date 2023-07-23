453321d8e5156644cc344635155f1855043c553c
olaguru <dapoodedeyi03@gmail.com>
2023-07-23 11:07:12 -0700

    my header file

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int my_putchar(char);
+int dee_puts(char *string);
+#endif /* _MAIN_H_ */
