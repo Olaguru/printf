03b15380c050319a83c5c200ecc2fe6ab11b7a0d
olaguru <dapoodedeyi03@gmail.com>
2023-07-23 11:06:19 -0700

    print f handling c and s perfectly remaining %

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf - a function that produces output according to a format
+ *
+ * @format: format for the output
+ *
+ * Return: int
+ */
+int _printf(const char *format, ...)
+{
+	int my_printf_return = 0;
+	unsigned int it;
+	int s_add;
+
+	va_list myargs;
+	va_start(myargs, format); 
+
+	for (it = 0; format[it] != '\0'; it++)
+	{
+		if (format[it] != '%')
+		{
+			my_putchar(format[it]);
+		}
+		else if (format[it] == '%' && format[it + 1] == 'c')
+		{
+			my_putchar(va_arg(myargs, int));
+			it++;
+		}
+		else if (format[it] == '%' && format[it + 1] == 's')
+		{
+			s_add = dee_puts(va_arg(myargs, char *));
+			it++;
+			my_printf_return += (s_add - 1);
+		}
+		else if (format[it] == '%' && format[it + 1] == '%')
+		{
+			my_putchar(format[it]);
+			it++;
+		}
+		my_printf_return += 1;
+	}
+	va_end(myargs);
+	return (my_printf_return);
+
+}
