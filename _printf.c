9bf71b3e31b840462e5d03e92fc8a183b4890e0d
olaguru <dapoodedeyi03@gmail.com>
2023-07-24 18:30:16 -0700

    update

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int my_printf_return = 0;
+	int p_ret = 0;
 	unsigned int it;
 	int s_add;
 
@@ -22,23 +22,27 @@ int _printf(const char *format, ...)
 	{
 		if (format[it] != '%')
 		{
-			my_putchar(format[it]);
+			p_ret += my_putchar(format[it]);
 		}
-		else if (format[it] == '%' )
+		else if (format[it + 1] == '%' )
+		{
+			p_ret += my_putchar('%');
+			it++;
+		}
 		else if (format[it] == '%' && format[it + 1] == 'c')
 		{
-			my_putchar(va_arg(myargs, int));
+			p_ret += my_putchar(va_arg(myargs, int));
 			it++;
 		}
 		else if (format[it] == '%' && format[it + 1] == 's')
 		{
 			s_add = dee_puts(va_arg(myargs, char *));
 			it++;
-			my_printf_return += (s_add - 1);
+			p_ret += (s_add - 1);
 		}
-		my_printf_return += 1;
+
 	}
 	va_end(myargs);
-	return (my_printf_return);
+	return (p_ret);
 
 }
