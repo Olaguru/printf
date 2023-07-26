2987c0df4e4553e9480bbfe7f1c1d9423e6f7b8f
olaguru <dapoodedeyi03@gmail.com>
2023-07-25 19:06:47 -0700

    test 0

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int my_putchar(char ch);
+int dee_puts(char *st);
+#endif
